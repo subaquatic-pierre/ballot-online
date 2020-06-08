7661b9c110be0a5ee76bb5fd7a9e57fc61365048
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-06-08 21:23:30 +0400

    update log files

--- backend/app/gunicorn.config.py
+++ backend/app/gunicorn.config.py
@@ -5,4 +5,6 @@ bind = '127.0.0.1:8000'
 workers = 4
 worker_class = 'gevent'
 
-accesslog = '-'
+accesslog = '/home/ubuntu/log/gunicorn-access.log'
+errorlog = '/home/ubuntu/log/gunicorn-error.log'
+loglevel = 'debug'
